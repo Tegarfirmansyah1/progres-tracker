7e89c64029f4a1de5831097f467e402f328570ad
Tegarfirmansyah1 <tegarf555@gmail.com>
2026-05-07 01:09:29 +0700

    feat: implement getDashboardData function to fetch user progress bars from Supabase

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,6 +1,23 @@
 // src/lib/actions.ts
+'use server';
+
+import { createClient } from '@supabase/supabase-js';
 import { supabase } from './supabase';
 import { calculateProgress } from './utils';
+
+const supabaseUrl = process.env.SUPABASE_SERVICE_ROLE_URL; 
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+export async function getDashboardData() {
+  const supabase = createClient(supabaseUrl!, supabaseKey!);
+  
+  const { data, error } = await supabase
+    .from('user_progress_bars')
+    .select('*');
+
+  if (error) throw new Error(error.message);
+  return data;
+}
 
 export const addDynamicLog = async (
   activityType: 'cycling' | 'reading' | 'coding',
